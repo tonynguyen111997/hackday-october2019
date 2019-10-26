859f887ace3fa76d9a00579ffbcf529001a60475
Ryan Kelley <rdkelley@gmail.com>
2019-10-26 16:51:55 -0700

    Add html route, html

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const path = require("path");
 
 const app = express();
 
-const PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('IT works');
+app.use(express.static("views"));
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./views/index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+  console.log(`Server is running on port ${PORT}`);
 });
